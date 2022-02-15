26dd7cf59f73e2661e3772197a3c1e6e7b971035
Tuhinbd71 <m.tuhin02bd@gmail.com>
2022-02-15 23:27:09 +0600

    bank commit

---
+++ js/banking.js
@@ -0,0 +1,15 @@
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+
+    const depositAmount = depositInput.value;
+
+
+    // get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositAmount;
+
+    // clear the deposit input field
+    depositInput.value = '';
+
+})
